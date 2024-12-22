2dd3d6b71f4d640c34b6bcf0ebe86bf7e4122211
Sivkim.HENG <hengsivkim14@gmail.com>
2024-12-22 21:26:18 +0700

    10:24pm fix quizcard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,18 +1,14 @@
 <x-app-layout>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <x-card>
-                      <div class="card-body">
-                        <h2 class="card-title">Test</h2>
-                        <p>Good boy play game lol</p>
-                        <div class="card-actions justify-end">
-                          <button class="btn">Play Now</button>
-                        </div>
-                      </div>
-                    </x-card>
+
+
+
+
+
+
                 </div>
             </div>
         </div>
